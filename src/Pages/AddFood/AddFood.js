3b489b2d5071f6ea1918a40801189ae068ad8331
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2022-02-20 11:43:13 +0600

    form data showup

--- src/Pages/AddFood/AddFood.js
+++ src/Pages/AddFood/AddFood.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import Footer from '../Footer/Footer';
+import Navbar from '../Home/Navbar';
 import './AddFood.css';
 
-import { useForm } from "react-hook-form";
-import Navbar from '../Home/Navbar';
-import Footer from '../Footer/Footer';
 
 const AddFood = () => {
     const { register, formState: { errors }, reset, handleSubmit } = useForm();
@@ -31,15 +31,19 @@ const AddFood = () => {
             <h1 className="order-tittle">Add Food</h1>
             <div className="add-food">
                 <form onSubmit={handleSubmit(onSubmit)}>
+                    <p>Tittle</p>
                     <input {...register("tittle", { required: true })} placeholder="tittle" />
                     {errors.tittle?.type === 'required' && "title is required"}
 
+                    <p>Description</p>
                     <input {...register("description", { required: true })} placeholder="description" />
                     {errors.description && "description is required"}
 
+                    <p>Price</p>
                     <input type="number" {...register("price")} placeholder="price" />
                     {errors.price && "price is required"}
 
+                    <p>ImageUrl</p>
                     <input {...register("imageUrl", { required: true })} placeholder="imageUrl" />
                     {errors.imageUrl?.type === 'required' && "imageUrl is required"}
 
